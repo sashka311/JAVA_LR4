916ba32b76af6837fcf35bec1d47421d1e572081
sashka311 <ihor.garashchenko@gmail.com>
2022-10-20 07:19:23 +0300

    complete lr4

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,20 @@
 public class Main {
     public static void main(String[] args) {
-        //foo
+        //task1
+        System.out.println(Character.isLetter('S'));
 
+        //task 2
+        char emoji1 = '\u2639';
+        String emoji2 = "\uD83D\uDE16";
+        String emoji3 = "\ud83d\ude0d";
+        System.out.println("3 Смайлика:  " + emoji1 + "  " +emoji2 + "  " + emoji3 );
+
+        //task3
+        String str1 = "Java";
+        String str2 = "C++";
+
+        System.out.println("str1 = "+ str1+ " str2 = "+str2);
+        System.out.println("str1 == str2 -> "+ str1.compareTo(str2));
+        System.out.println("str2 == str1 -> "+ str2.compareTo(str1));
     }
 }
